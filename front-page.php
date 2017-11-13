a9081da328d29f4c7de61939debe7f64ae2823a5
David Suh <dsmuchu@gmail.com>
2017-11-13 02:29:09 +0100

    finished ch38 - Professor post Type

--- front-page.php
+++ front-page.php
@@ -6,7 +6,7 @@
       <h1 class="headline headline--large">Welcome!</h1>
       <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
       <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-      <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+      <a href="<?php echo get_post_type_archive_link('program'); ?>" class="btn btn--large btn--blue">Find Your Major</a>
     </div>
   </div>
 
@@ -16,9 +16,21 @@
         <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
         <?php
+        $today = date('Ymd');
         $homePageEvents = new WP_QUERY(array(
           'posts_per_page' => 2,
           'post_type' => 'event',
+          'meta_key' => 'event_date',
+          'orderby' => 'meta_value_num',
+          'order' => 'ASC',
+          'meta_query' => array(
+            array(
+              'key' => 'event_date',
+              'compare' => '>=',
+              'value' => $today,
+              'type' => 'numeric'
+            )
+          )
         ));
 
         while ($homePageEvents -> have_posts()) {
@@ -96,7 +108,7 @@
     <div class="hero-slider__interior container">
       <div class="hero-slider__overlay">
         <h2 class="headline headline--medium t-center">Free Food</h2>
-        <p class="t-center">Fictional University offers lunch plans for those in need.</p>
+        <p class="t-center">Amazing College offers lunch plans for those in need.</p>
         <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
       </div>
     </div>
--- index.php
+++ index.php
@@ -12,10 +12,8 @@ get_theme_file_uri('/images/ocean.jpg'); ?>);">
 </div>
 
 <div class="container container--narrow page-section">
-
 <?php
-
-while(have_posts()) {
+  while(have_posts()) {
   the_post(); ?>
 <div class="post-item">
   <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
@@ -29,7 +27,7 @@ while(have_posts()) {
 </div>
    <?php
 }
-echo paginate_links(); 
+echo paginate_links();
  ?>
 
 </div>
